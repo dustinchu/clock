81d758570d694f40978698279c0283a275648886
wisplu <dustin_chu@hotmail.com>
2019-01-30 23:37:51 +0800

    change procfile

--- clock.py
+++ clock.py
@@ -5,7 +5,7 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
 def scheduled_job():
-    r = requests.get('https://www.google.com.tw/')
+    r = requests.get('https://bankyou.herokuapp.com/news')
     if r.status_code == requests.codes.ok:
         print("OK")
 sched.start()
