ff5e724698139e11def487576f05ba34b4511b58
wisplu <dustin_chu@hotmail.com>
2019-02-01 19:59:01 +0800

    edit clock

--- clock.py
+++ clock.py
@@ -1,23 +1,14 @@
-# from apscheduler.schedulers.blocking import BlockingScheduler
-# import requests
-#
-# sched = BlockingScheduler()
-#
-# @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
-# def scheduled_job():
-#     r = requests.get('https://bankyou.herokuapp.com/news')
-#     if r.status_code == requests.codes.ok:
-#         print("OK")
-#
-# sched.start()
 
 from apscheduler.schedulers.blocking import BlockingScheduler
-
+import requests
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=3)
+@sched.scheduled_job('interval', minutes=59)
 def timed_job():
-    print('This job is run every three minutes.')
+    r = requests.get('https://bankyou.herokuapp.com/news')
+    if r.status_code == requests.codes.ok:
+        print("OK")
+    print('This job is run every 59 minutes.')
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
 def scheduled_job():
