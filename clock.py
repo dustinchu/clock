2d4233647f72087b7aae755ab9328e781748634a
wisplu <dustin_chu@hotmail.com>
2019-02-01 21:01:51 +0800

    edit clock

--- clock.py
+++ clock.py
@@ -6,12 +6,9 @@ sched = BlockingScheduler()
 @sched.scheduled_job('interval', minutes=59)
 def timed_job():
     r = requests.get('https://bankyou.herokuapp.com/news')
-    if r.status_code == requests.codes.ok:
-        print("OK")
-    print('This job is run every 59 minutes.')
-
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
-def scheduled_job():
-    print('This job is run every weekday at 5pm.')
+    print(r.text)
+# @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
+# def scheduled_job():
+#     print('This job is run every weekday at 5pm.')
 
 sched.start()
